72961f3c1d04a3f303c7b4cb16fa61f996f584b5
quantummik <quantummik@naver.com>
2025-05-05 01:52:49 +0900

    ymi pytest -v -s  추가,  test adjustment

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 python --version : 3.10.10
 
+pytest는 test_*.py 형식의 파일과 test_로 시작하는 함수를 자동으로 찾아 실행함
+
+# Project Structure
+
+```plaintext
 my-app/
 ├── app.py                   # Flask 애플리케이션 진입점
 ├── requirements.txt         # 필요한 패키지 목록
--- test_app.py
+++ test_app.py
@@ -1,6 +1,10 @@
+# test_app.py
 from app import app
 
 def test_home():
     client = app.test_client()
     response = client.get("/")
+    
+    print("success")
+    assert response.status_code == 200
     assert response.data == b"Hello, CI/CD!"
