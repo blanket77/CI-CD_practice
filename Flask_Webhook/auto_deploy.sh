97dc8610be5876b35e1e1ded17522a24dba4a57b
quantummik <quantummik@naver.com>
2025-05-06 10:20:00 +0900

    test_4

--- Flask_Webhook/auto_deploy.sh
+++ Flask_Webhook/auto_deploy.sh
@@ -7,7 +7,7 @@ docker pull $IMAGE
 
 echo "🧹 Cleaning up old container..."
 docker stop flask-app || true # Stop the container if it's running
-docker rm flask-app || true # Remove the container if it exists
+docker rm flask-app || true # Remove the container if it exists!
 
 echo "🔥 Running new container..." 
 docker run -d --name flask-app -p 8080:5000 $IMAGE
