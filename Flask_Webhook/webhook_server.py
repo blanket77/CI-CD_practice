e674ec536b1f2ec3ae0b0c887c1b9d9ed055c4e6
quantummik <quantummik@naver.com>
2025-05-06 10:09:25 +0900

    test_2

--- Flask_Webhook/webhook_server.py
+++ Flask_Webhook/webhook_server.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/hook', methods=['POST'])
 def webhook():
-    print("!! Webhook received from GitHub!")
-    os.system('./auto_deploy.sh')  # Docker 컨테이너 재시작 스크립트 
+    print("!! Webhook received from GitHub!!!")
+    os.system('C:/Users/quant.QUANT/Desktop/2025/CI_CD_practice/CI-CD_practice/Flask_Webhook/auto_deploy.sh')  # Docker 컨테이너 재시작 스크립트 
     # os.system이란 시스템 쉘에서 명령어를 실행하는 함수, cmd를 실행하는 것과 같음
     return '', 204
 
