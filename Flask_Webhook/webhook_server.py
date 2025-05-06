01fc9930634b6020459ef9239884627d62da9320
quantummik <quantummik@naver.com>
2025-05-06 10:40:16 +0900

    test_8

--- Flask_Webhook/webhook_server.py
+++ Flask_Webhook/webhook_server.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import os
 import subprocess
 
 app = Flask(__name__)
@@ -8,11 +7,18 @@ app = Flask(__name__)
 def webhook():
     bash_path = r"C:\Program Files\Git\bin\bash.exe"
     script_path = r"C:\Users\quant.QUANT\Desktop\2025\CI_CD_practice\CI-CD_practice\Flask_Webhook\auto_deploy.sh"
-    subprocess.run([bash_path, script_path])
-    print("!! Webhook received from GitHub!!!")
 
-    # os.system이란 시스템 쉘에서 명령어를 실행하는 함수, cmd를 실행하는 것과 같음
+    result = subprocess.run([bash_path, script_path], capture_output=True, text=True)
+    print("✅ Webhook received")
+
+    if result.returncode != 0:
+        print("❌ 실행 실패")
+        print(result.stderr)
+    else:
+        print("✅ 실행 성공")
+        print(result.stdout)
+
     return '', 204
 
-if __name__ == '__main__': # Flask 서버 실행
+if __name__ == '__main__':
     app.run(port=6000)
