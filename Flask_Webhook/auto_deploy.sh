54a0b83bddb3c739d4ba6349c2639239f496f726
quantummik <quantummik@naver.com>
2025-05-06 10:22:30 +0900

    test_5

--- Flask_Webhook/auto_deploy.sh
+++ Flask_Webhook/auto_deploy.sh
@@ -3,11 +3,11 @@
 IMAGE=blanket77/flask-cicd-app:latest # Replace with your Docker image name
 
 echo "Pulling image from DockerHub..."  
-docker pull $IMAGE 
+docker pull blanket77/flask-cicd-app:latest
 
 echo "🧹 Cleaning up old container..."
 docker stop flask-app || true # Stop the container if it's running
 docker rm flask-app || true # Remove the container if it exists!
 
 echo "🔥 Running new container..." 
-docker run -d --name flask-app -p 8080:5000 $IMAGE
+docker run -d --name flask-app -p 8080:5000 blanket77/flask-cicd-app:latest
